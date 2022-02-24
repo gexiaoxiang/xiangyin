0ca5005aaf96f70182ad8cb0ffb9924c0de78001
Apollo2016926 <944644308@qq.com>
2022-02-25 00:41:01 +0800

    完成项目的公共组件

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,14 +1,24 @@
 <template>
-  <div  >
-    我是根
-  </div>
+    <div>
+
+        <Header></Header>
+        根组件 制作图标
+        <Footer></Footer>
+
+    </div>
 </template>
 
 <script>
+    import Header from './components/Header'
+    import Footer from './components/Footer'
 
-export default {
-  name: 'App'
-}
+    export default {
+        name: 'App',
+        components: {
+            Header,
+            Footer
+        }
+    }
 </script>
 
 <style>
